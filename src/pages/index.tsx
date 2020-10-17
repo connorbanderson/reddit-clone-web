4c89a122655732464c778081d0c091d154d97030
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 17:58:10 -0600

    Connect Index with withUrqlClient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
+
 const Index = () => {
+  const [{ data }] = usePostsQuery();
   return (
     <section>
+      <NavBar />
       <span>hello world</span>
+      {data && data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
     </section>
   );
 };
 
-export default Index;
+export default withUrqlClient(createUrqlClient)(Index);
