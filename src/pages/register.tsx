42e3b9a62b33544764ea428efc7ce6574a617983
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 10:52:28 -0600

    Register Page Update

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import { FormControl, FormLabel, Input, Box, Button } from "@chakra-ui/core";
+import { Box, Button } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const [{}, register] = useRegisterMutation();
   return (
     <Wrapper>
       <Formik
-        onSubmit={(e) => console.log("onSubmit", e)}
-        initialValues={{ userName: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          const errors = response.data?.register.errors;
+          if (errors) {
+            setErrors(toErrorMap(errors));
+          }
+        }}
+        initialValues={{ username: "", password: "" }}
       >
         {({ isSubmitting }) => (
           <Form>
