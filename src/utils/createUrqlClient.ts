73efadd20d00983dc77b63466d05840cdc915966
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 21:39:37 -0600

    createUrqlClient - errorExchange to handle unauthed users from posting

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,5 +1,8 @@
-import { dedupExchange, fetchExchange } from "urql";
+import { dedupExchange, fetchExchange, Exchange } from "urql";
+import { filter, pipe, tap } from "wonka";
 import { cacheExchange } from "@urql/exchange-graphcache";
+import Router from "next/router";
+
 import {
   MeDocument,
   LoginMutation,
@@ -8,6 +11,17 @@ import {
   LogoutMutation,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+
+const errorExchange: Exchange = ({ forward }) => (ops$) => {
+  return pipe(
+    forward(ops$),
+    tap(({ error }) => {
+      if (error?.message.includes("not authenticated")) {
+        Router.replace("/login");
+      }
+    })
+  );
+};
 
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:4000/graphql",
@@ -66,6 +80,7 @@ export const createUrqlClient = (ssrExchange: any) => ({
         },
       },
     }),
+    errorExchange,
     ssrExchange,
     fetchExchange,
   ],
