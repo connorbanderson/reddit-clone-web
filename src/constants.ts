2a05cf85d3f523b5220e0afe31b5c1a749ff8a5a
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 22:34:59 -0600

    ColorScheme

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const colorSchemeUrl =
+  "https://digitalsynopsis.com/design/minimal-web-color-palettes-combination-hex-code/";
+export const colorScheme = {
+  orange: "#F8B195",
+  salmon: "#F67280",
+  lightPurple: "#C06C84",
+  darkPurple: "#6C5B7B",
+  blue: "#355C7D",
+};
