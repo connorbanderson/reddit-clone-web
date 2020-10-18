02c1a4c9af597998a15bf9995245533acdd06df7
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 21:41:05 -0600

    Create a Post Button Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
+import { Button, Flex } from "@chakra-ui/core";
+
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { useRouter } from "next/router";
 
 const Index = () => {
+  const router = useRouter();
+
   const [{ data }] = usePostsQuery();
+
   return (
-    <section>
-      <NavBar />
-      <span>hello world</span>
-      {data && data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
-    </section>
+    <Layout>
+      <Flex pl="4" pr="4" width="100%" justifyContent="flex-end">
+        <Button onClick={() => router.push("/create-post")}>
+          Create A Post
+        </Button>
+      </Flex>
+      {data?.posts.map((p) => (
+        <div key={p.id}>{p.title}</div>
+      ))}
+    </Layout>
   );
 };
 
