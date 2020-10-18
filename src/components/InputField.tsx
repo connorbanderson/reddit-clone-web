46ce56bae99a73c68ba2ddb736a63ec1f3c80789
Connor Anderson <connorbanderson@gmail.com>
2020-10-17 21:42:48 -0600

    InputFIeld - Conditional textarea prop

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,23 +5,27 @@ import {
   FormLabel,
   FormErrorMessage,
   Input,
+  Textarea,
 } from "@chakra-ui/core";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea = false,
   size: _,
   ...props
 }) => {
+  const Component = !textarea ? Input : Textarea;
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <Component {...field} {...props} id={field.name} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
