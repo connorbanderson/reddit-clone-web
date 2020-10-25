3e2619c2074c7088ffe2a620ff221fca09ee9eed
Connor Anderson <connorbanderson@gmail.com>
2020-10-24 23:32:26 -0600

    Update useIsAuth.ts

--- src/utils/useIsAuth.ts
+++ src/utils/useIsAuth.ts
@@ -3,11 +3,11 @@ import { useRouter } from "next/router";
 import { useMeQuery } from "../generated/graphql";
 
 const useIsAuth = () => {
-  const [{ data, fetching }] = useMeQuery();
+  const { data, loading } = useMeQuery();
   const router = useRouter();
   useEffect(() => {
-    !fetching && !data?.me && router.replace("/login?next=" + router.pathname);
-  }, [router, data]);
+    !loading && !data?.me && router.replace("/login?next=" + router.pathname);
+  }, [loading, router, data]);
 };
 
 export { useIsAuth };
