55ab15ed7770a1a0b8db6b4543ab0f68545a6ab5
Connor Anderson <connorbanderson@gmail.com>
2020-10-24 23:32:04 -0600

    Update create-post.tsx

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import { Box, Button } from "@chakra-ui/core";
+import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
-import { Formik, Form } from "formik";
-import { Box, Button } from "@chakra-ui/core";
-
+import React from "react";
 import { InputField } from "../components/InputField";
 import { Layout } from "../components/Layout";
+import { useCreatePostMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { useIsAuth } from "../utils/useIsAuth";
-import { useCreatePostMutation } from "../generated/graphql";
+import { withApollo } from "../utils/withApollo";
 
 const CreatePost: React.FC<{}> = ({}) => {
   const router = useRouter();
   useIsAuth();
-  const [{}, createPost] = useCreatePostMutation();
+  const [createPost] = useCreatePostMutation();
   return (
     <Layout variant="small">
       <Formik
-        onSubmit={async (values) => {
-          const { error } = await createPost({ input: values });
-          !error && router.push("/");
-        }}
         initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          const { errors } = await createPost({
+            variables: { input: values },
+            update: (cache) => {
+              cache.evict({ fieldName: "posts:{}" });
+            },
+          });
+          if (!errors) {
+            router.push("/");
+          }
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -30,19 +37,18 @@ const CreatePost: React.FC<{}> = ({}) => {
               <InputField
                 textarea
                 name="text"
-                placeholder="test..."
+                placeholder="text..."
                 label="Body"
               />
             </Box>
-            <Box mt={6}>
-              <Button
-                type="submit"
-                isLoading={isSubmitting}
-                variantColor="twitter"
-              >
-                Create Post
-              </Button>
-            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              create post
+            </Button>
           </Form>
         )}
       </Formik>
@@ -50,4 +56,4 @@ const CreatePost: React.FC<{}> = ({}) => {
   );
 };
 
-export default withUrqlClient(createUrqlClient)(CreatePost);
+export default withApollo({ ssr: false })(CreatePost);
